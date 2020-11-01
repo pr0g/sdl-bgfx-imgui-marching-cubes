f1f092e95b868e78adf416d19f65ea0abe326359
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-11-01 15:27:51 +0000

    update configure/build scripts

--- compile-shaders.bat
+++ compile-shaders.bat
@@ -2,7 +2,16 @@
 
 REM compiler shaders
 
-REM normal shader
+REM simple shader
+third-party\libs\bgfx\install\bin\shaderc.exe ^
+-f shader\simple\v_simple.sc -o shader\simple\v_simple.bin ^
+--platform windows --type vertex --verbose -i ./ -p vs_5_0
+
+third-party\libs\bgfx\install\bin\shaderc.exe ^
+-f shader\simple\f_simple.sc -o shader\simple\f_simple.bin ^
+--platform windows --type fragment --verbose -i ./ -p ps_5_0
+
+REM next (normal) shader
 third-party\libs\bgfx\install\bin\shaderc.exe ^
 -f shader\next\v_next.sc -o shader\next\v_next.bin ^
 --platform windows --type vertex --verbose -i ./ -p vs_5_0
