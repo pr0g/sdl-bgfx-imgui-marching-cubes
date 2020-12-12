642f2cc88d591eaf7c0cdc2be70a8b685469cf1c
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-12 20:07:10 +0000

    update repo to use new third-party CMake build using ExternalProject_Add

--- compile-shaders.bat
+++ compile-shaders.bat
@@ -3,19 +3,19 @@
 REM compiler shaders
 
 REM simple shader
-third-party\libs\bgfx\install\bin\shaderc.exe ^
+third-party\build\bin\shaderc.exe ^
 -f shader\simple\v_simple.sc -o shader\simple\v_simple.bin ^
 --platform windows --type vertex --verbose -i ./ -p vs_5_0
 
-third-party\libs\bgfx\install\bin\shaderc.exe ^
+third-party\build\bin\shaderc.exe ^
 -f shader\simple\f_simple.sc -o shader\simple\f_simple.bin ^
 --platform windows --type fragment --verbose -i ./ -p ps_5_0
 
 REM next (normal) shader
-third-party\libs\bgfx\install\bin\shaderc.exe ^
+third-party\build\bin\shaderc.exe ^
 -f shader\next\v_next.sc -o shader\next\v_next.bin ^
 --platform windows --type vertex --verbose -i ./ -p vs_5_0
 
-third-party\libs\bgfx\install\bin\shaderc.exe ^
+third-party\build\bin\shaderc.exe ^
 -f shader\next\f_next.sc -o shader\next\f_next.bin ^
 --platform windows --type fragment --verbose -i ./ -p ps_5_0
