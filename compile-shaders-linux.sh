48037807bca39e460adb9c31f6eb5182921fced5
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-31 16:02:17 +0000

    add support for linux

--- compile-shaders-linux.sh
+++ compile-shaders-linux.sh
@@ -5,17 +5,17 @@
 # simple shader
 ./third-party/build/bin/shaderc \
 -f shader/simple/v_simple.sc -o shader/simple/v_simple.bin  \
---platform osx --type vertex --verbose -i ./ -p metal
+--platform linux --type vertex --verbose -i ./
 
 ./third-party/build/bin/shaderc \
 -f shader/simple/f_simple.sc -o shader/simple/f_simple.bin \
---platform osx --type fragment --verbose -i ./ -p metal
+--platform linux --type fragment --verbose -i ./
 
 # next (normal) shader
 ./third-party/build/bin/shaderc \
--f shader/next/v_next.sc -o shader/next/v_next.bin  \
---platform osx --type vertex --verbose -i ./ -p metal
+-f shader/next/v_next.sc -o shader/next/v_next.bin \
+--platform linux --type vertex --verbose -i ./
 
 ./third-party/build/bin/shaderc \
 -f shader/next/f_next.sc -o shader/next/f_next.bin \
---platform osx --type fragment --verbose -i ./ -p metal
+--platform linux --type fragment --verbose -i ./
